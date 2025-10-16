06df0bc89bcd6e0316e1794a4e2146a74a5dc003
k24-0574 <k240574@nu.edu.pk>
2025-10-16 20:59:06 +0500

    Add files via upload

---
+++ Task2lab6.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+#include <ctime>
+
+class Transaction{
+    int ID;
+    int amount;
+    string description;
+    bool flag;
+    public:
+    Transaction(int a, string d){
+        ID = 0;
+        amount =a;
+        description = d;
+        if(amount >=0) flag=1;
+        else flag=0;
+    }
+    void setID(int ID){
+        this->ID = ID;
+    }
+    void checkDescription(){
+        if(description.length()>20){
+            description = description.substr(0, 17) + "...";
+        }
+    }
+
+    void applyDiscount(){
+        if(!flag) return;
+        
+        if(amount>=1500){
+            amount*=0.7;
+        }
+        else if(amount>=1000){
+            amount *=0.85;
+        }
+        else if(amount>=500){
+            amount *=0.95;
+        }
+    }
+
+    int getAmount(){
+        return amount;
+    }
+    void setAmount(int a){
+        amount =a;
+    }
+    void reverse(){
+        if(flag) return;
+        flag= true;
+        amount *=-1;
+        description = description + "[REVERSED]";
+    }
+
+    void display(){
+        cout<<"ID: "<<ID<<endl;
+        cout<<"Amount: "<< amount <<endl;
+        cout<<"Description:  "<<description<<endl;
+        cout<<"Flag: "<<flag<<endl; 
+        
+    }
+};
+
+
+class Node{
+    public:
+    Node* next;
+    Transaction *data;
+    Node(Transaction *t){
+        data = t;
+        next = nullptr;
+    }
+    ~Node(){
+        delete data;
+    }
+
+};
+class Stack{
+    static int AllID;
+
+    Node* head;
+    Node* tail;
+
+    public:
+    Stack(){
+        head= tail = nullptr;
+    }
+    ~Stack(){
+        Node*temp = head, *temp2;
+        while(temp){
+            temp2 = temp->next;
+            delete temp;
+            temp = temp2;
+        }
+    }
+
+    void push(Transaction *t){
+        t->setID(AllID++);
+        t->checkDescription();
+        t->applyDiscount();
+        Node* newnode = new Node(t);
+        if(isEmpty()){
+            head = tail = newnode;
+        }
+        else{
+            newnode->next = head;
+            head = newnode;
+        } 
+    }
+    void pop(){
+        if(isEmpty()) return;
+        Node *temp = head;
+        head = head->next;
+        temp->data->reverse();
+        delete temp;
+    }
+    Transaction* seektop(){
+        return head->data;
+    }
+    void display(){
+        Node* temp = head;
+        while(temp){
+            temp->data->display();
+            cout<<endl;
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+
+    bool isEmpty(){
+        return head==nullptr;
+    }
+};
+int Stack:: AllID = 1;
+
+int main(){
+    int itemPushed[4] = {0};
+    srand(time(0));
+    int numItemsPushed=0;
+    int randomIndex;
+
+    Transaction **Items = new Transaction*[4];
+    Items[0] = new Transaction(1200, "Sale: Blue Jacket");
+    Items[1] = new Transaction(450, "Sale: Cotton Socks");
+    Items[2] = new Transaction(-300, "Refund: Defective Shirt");
+    Items[3] = new Transaction(1700, "Sale: Leather Jacket");
+
+    Stack store;
+    while(numItemsPushed<4){
+        randomIndex = rand() % 4;
+        if(itemPushed[randomIndex]==0){
+            itemPushed[randomIndex]=1;
+            store.push(Items[randomIndex]);
+            numItemsPushed++;
+        }
+    }
+
+    store.display();
+
+
+    for(int i=0;i<4;i++) delete Items[i];
+    delete[] Items;
+
+    return 0;
+}
